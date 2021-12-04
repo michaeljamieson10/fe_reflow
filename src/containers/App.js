cd74a642373ce30ac0c6dce9b92ec6d76caaecf2
michaeljamieson <michaeljamieson10@gmail.com>
2021-12-04 14:22:49 -0500

    returned to normal class will keep it this way works need to get header to work with class

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,18 @@
-
-
-import React, {Component, useEffect, useState} from "react"
+import React, { Component } from "react"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
-import {withRouter, Route, Switch, Redirect, BrowserRouter} from "react-router-dom"
+import { ThemeProvider } from "@material-ui/styles";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom"
 import theme from "../components/ui/Theme";
-import {useIsMount} from "../hooks/useIsMount";
 
-import {MuiThemeProvider, createMuiTheme, ThemeProvider} from "@material-ui/core/styles"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import red from "@material-ui/core/colors/red"
 
 import { getAppAccessToken, getAccessTokenType, ACCESS_TOKEN_TYPES } from "../actions/oauthActions"
 import { updateLoggedInUserInfo } from "../actions/userActions"
 
+// import Header from "../components/Header";
 
 import SignUp from "./SignUp"
 // import Login from "./Login"
@@ -24,73 +23,105 @@ import Login from "./Login";
 import ClientForm from "./ClientForm";
 import ClientManagement from "./ClientManagement";
 import Header from "../components/ui/Header";
-import Footer from "../components/ui/Footer";
-// import LandingPage from "../components/LandingPage";
 
+// const theme = createMuiTheme({
+//   overrides: {
+//     MuiButton: {
+//       root: {
+//         margin: "5px",
+//         padding: "10px"
+//       }
+//     }
+//   },
+//   layout: {
+//     width: "100%",
+//     height: "100%"
+//   },
+//   palette: {
+//     primary: {
+//       main: "#67cb33",
+//       contrastText: "#fff"
+//     },
+//     secondary: {
+//       main: "#000",
+//       contrastText: "#fff"
+//     },
+//     error: red
+//   },
+//   typography: {
+//     useNextVariants: true
+//   }
+// })
 
-function App(props) {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [showInstallMessage, setShowInstallMessage] = useState(false);
-  const [value, setValue] = useState(0);
-
-  const isMount = useIsMount();
-  useEffect(() =>{
-    if (props.accessToken === "") {
-      props.getAppAccessToken()
+class App extends Component {
+  // const [selectedIndex, setSelectedIndex] = useState(0);
+  // const [value, setValue] = useState(0);
+  constructor(props) {
+    super(props);
+    // this.setValue = this.setValue.bind(this);
+    this.state = {
+      showInstallMessage: false,
+      selectedIndex: 0,
+      value: 0,
     }
-    if (props.loggedInUserId > 0) {
-      props.pollLoggedInUser()
-    }},[]); //  empty array will cause a render only once
+    // this.setState({ [stateName + "State"]: "success" });
+  }
+  // setValue = (val) => {
+  //   this.setState({value: val
+  //   })}
+  // setSelectedIndex = (idx) =>{
+  //   this.setState({
+  //     selectedIndex: idx
+  //   })
+  // }
 
-
-  useEffect( (newProps) => {
-    if(!isMount) {
-      if (
-          newProps.accessToken !== "" &&
-          newProps.accessToken !== props.accessToken &&
-          getAccessTokenType() === ACCESS_TOKEN_TYPES.user
-      ) {
-        pollLoggedInUser()
-      }
+  componentDidMount() {
+    if (this.props.accessToken === "") {
+      this.props.getAppAccessToken()
     }
-
-  }, [props.accessToken])
-
-
-
-  const pollLoggedInUser = () => {
-    // this.props.updateLoggedInUserInfo()
-    props.updateLoggedInUserInfo()
-    //TODO: parametrize this timeout
-    // const timeout = setTimeout(this.pollLoggedInUser, 60000)
+    if (this.props.loggedInUserId > 0) {
+      this.pollLoggedInUser()
+    }
   }
 
-  // render() {
-  const { loggedInUser } = props
-  return (
-      <React.Fragment>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <Header
-                value={value}
-                setValue={setValue}
-                selectedIndex={selectedIndex}
-                setSelectedIndex={setSelectedIndex}
-            />
-            <Switch>
+  componentWillReceiveProps(newProps) {
+    if (
+        newProps.accessToken !== "" &&
+        newProps.accessToken !== this.props.accessToken &&
+        getAccessTokenType() === ACCESS_TOKEN_TYPES.user
+    ) {
+      this.pollLoggedInUser()
+    }
+  }
 
+  pollLoggedInUser = () => {
+    this.props.updateLoggedInUserInfo()
+    this.timeout = setTimeout(this.pollLoggedInUser, 60000)
+  }
+
+  render() {
+    const { loggedInUser } = this.props
+    return (
+        // <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <React.Fragment>
+            <CssBaseline />
+            {/*<Header loggedInUser={loggedInUser} selectedIndex={this.selectedIndex}*/}
+            {/*        setSelectedIndex={this.setSelectedIndex}*/}
+            {/*        value={this.value}*/}
+            {/*        setValue={this.setValue}*/}
+            {/*    />*/}
+            <Switch>
               {/*<Route path="/" component={SignUp}/>*/}
-              {/*<Route path="/" component={LandingPage} />*/}
               <Route path="/login" component={Login} />
               <Route path="/client" component={ClientManagement} />
               <Route path="/signup" component={SignUp} />
             </Switch>
-            {/*<Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />*/}
-          </BrowserRouter>
+
+          </React.Fragment>
         </ThemeProvider>
-      </React.Fragment>
-  )
-  // }
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
